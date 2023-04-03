28cadb2260097f75e71874df55cb4377e4c60b6e
estebanhramirez <128089275+estebanhramirez@users.noreply.github.com>
2023-04-03 00:51:31 +0000

    added run.py file

--- PythonCode/Ngram/run.py
+++ PythonCode/Ngram/run.py
@@ -1,12 +1,5 @@
 from count_n_grams import count_n_grams
-from make_count_matrix import make_count_matrix
-
-def make_probability_matrix(n_plus1_gram_counts, vocabulary, k=1.0):
-    count_matrix = make_count_matrix(n_plus1_gram_counts, vocabulary)
-    count_matrix += k
-    prob_matrix = count_matrix.div(count_matrix.sum(axis=1), axis=0)
-    return prob_matrix
-
+from make_probability_matrix import make_probability_matrix
 
 def run():
     previous_n_gram_length = 3
@@ -24,6 +17,6 @@ def run():
     print(probability_matrix.loc[[('b', 'a', 'a')]])
     print(probability_matrix.loc[[('<s>', '<s>', '<s>')]])
     print()
-    print(probability_matrix.loc[[('<s>', '<s>', '<s>')]]['a'])
+    print(probability_matrix.loc[[('<s>', '<s>', '<s>')]]['<unk>'])
 
-#run()
+run()
