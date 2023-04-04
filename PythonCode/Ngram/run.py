c3d7a1fcabe6c621346f6efce5fd2a2bc8568baa
estebanhramirez <128089275+estebanhramirez@users.noreply.github.com>
2023-04-04 02:44:41 +0000

    stochastic reproducible extension almost ready

--- PythonCode/Ngram/run.py
+++ PythonCode/Ngram/run.py
@@ -17,15 +17,11 @@ def find(search_buffer, unique_words, probability_matrix):
     return maxword, maxprob
 
 
-def extend(search_buffer, unique_words, probability_matrix):
-    print()
-
-
 def naive_extend(search_buffer_ini, sentences, unique_words):
     search_buffer = search_buffer_ini
     nextword = '' 
     extension = ''
-    while nextword != '<e>' and extension in search_buffer_ini:
+    while nextword != '<e>' and extension in search_buffer_ini+extension[:-1]:
         previous_n_gram_length = len(search_buffer)
 
         n_plus1_gram_counts = count_n_grams(sentences, previous_n_gram_length+1)
@@ -35,11 +31,11 @@ def naive_extend(search_buffer_ini, sentences, unique_words):
         nextword, _ = find(search_buffer, unique_words, probability_matrix)
         search_buffer += nextword
         extension += nextword
-    print(search_buffer_ini+'|'+extension[:-1])
+    print(search_buffer_ini+'|'+extension[:])
 
 
 def entropy_naive_extend(sentences, unique_words):
-    previous_n_gram_length = 3
+    previous_n_gram_length = 4
     n_plus1_gram_counts = count_n_grams(sentences, previous_n_gram_length+1)
     probability_matrix = make_probability_matrix(n_plus1_gram_counts, unique_words, k=0)
     for i in probability_matrix.index:
@@ -50,13 +46,12 @@ def entropy_naive_extend(sentences, unique_words):
 def k_permutations(unique_words, k):
     return ([''.join(x) for x in product(unique_words, repeat=k)])
 
-def run():
-    sentences = [['a', 'a', 'b', 'a', 'b', 'b', 'b', 'b', 'a', 'a', 'b', 'a', 'b'],
-                 ['b', 'b', 'a', 'b', 'a', 'a', 'a', 'a', 'b', 'b', 'a', 'b', 'a']]
-    unique_words = list(set(sentences[0]+sentences[1]))
-    search_buffer_ini = 'baa'
 
-    naive_extend(search_buffer_ini, sentences, unique_words)
+def run():
+    sentences = [['a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b']] #[['a', 'a', 'b', 'a', 'b', 'b', 'b', 'b', 'a', 'a', 'b', 'a', 'b'], ['b', 'b', 'a', 'b', 'a', 'a', 'a', 'a', 'b', 'b', 'a', 'b', 'a']]
+    unique_words = list(set(sentences[0]))
+    #search_buffer_ini = 'baa'
+    #naive_extend(search_buffer_ini, sentences, unique_words)
     entropy_naive_extend(sentences, unique_words)
 
 run()
