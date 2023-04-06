c5d46d4b9e812bd3a43702dc2f6b4f22514f8ba4
estebanhramirez <128089275+estebanhramirez@users.noreply.github.com>
2023-04-06 00:48:26 +0000

    finding the most probable preffix

--- PythonCode/Ngram/run.py
+++ PythonCode/Ngram/run.py
@@ -56,20 +56,63 @@ def k_permutations(unique_words, k):
     return ([''.join(x) for x in product(unique_words, repeat=k)])
 
 
+
+
+
+
+def better_extend(search_buffer_ini, sentences, unique_words, k):
+    print()
+
+def entropy_better_extend(sentences, unique_words, k):
+    previous_n_gram_length = 3
+    n_plus1_gram_counts = count_n_grams(sentences, previous_n_gram_length+1)
+    probability_matrix = make_probability_matrix(n_plus1_gram_counts, unique_words, k=0)
+    print(probability_matrix)
+    for idx in probability_matrix.index:
+        if not '<s>' in list(idx) and not '<e>' in list(idx):
+            suffixes = []
+            for i in range(0, len(list(idx))):
+                suffixes.append(idx[i:])
+
+            for suffix in suffixes:
+                print(suffix, len(suffix))
+                in_probability_matrix = probability_matrix
+                previous_n_gram = idx
+                cnt = 0
+                word = suffix[cnt]
+                prob = in_probability_matrix.loc[[previous_n_gram]][word][0]
+                while prob > 0 and cnt < len(suffix):
+                    print('P(',word,'|',''.join(previous_n_gram),')=',prob)
+                    previous_n_gram += tuple(word)
+                    cnt += 1
+                    word = suffix[cnt]
+                    n_plus1_gram_counts = count_n_grams(sentences, previous_n_gram_length)
+                    in_probability_matrix = make_probability_matrix(n_plus1_gram_counts, unique_words, k)
+                    if previous_n_gram in in_probability_matrix.index:
+                        prob = in_probability_matrix.loc[[previous_n_gram]][word][0]
+                    else:
+                        prob = 0
+                print('P(',word,'|',''.join(previous_n_gram),')=',0)
+            print()
+
+
+
+
 def run():
-    path = 'data/TXTs/dummy.txt'
-    dir_path = os.path.dirname(os.path.realpath(path))
-    for root, dirs, files in os.walk(dir_path):
-        for file in files:
-            with open(dir_path+'/'+file, 'r') as f:
-                txt = f.read()
-                txt = txt[0:1000]
-                sentences = [list(txt)] # [['a', 'b', 'a', 'a', 'b', 'a', 'a', 'b', 'a', 'a', 'b', 'a', 'a', 'b', 'a', 'a', 'b', 'a']] #[['a', 'a', 'b', 'a', 'b', 'b', 'b', 'b', 'a', 'a', 'b', 'a', 'b'], ['b', 'b', 'a', 'b', 'a', 'a', 'a', 'a', 'b', 'b', 'a', 'b', 'a']]
-                print(sentences)
-                unique_words = list(set(sentences[0]))
-                #search_buffer_ini = 'baa'
-                #naive_extend(search_buffer_ini, sentences, unique_words)
-                entropy_naive_extend(sentences, unique_words, k=1)
+    #path = 'data/TXTs/dummy.txt'
+    #dir_path = os.path.dirname(os.path.realpath(path))
+    #for root, dirs, files in os.walk(dir_path):
+    #    for file in files:
+    #        with open(dir_path+'/'+file, 'r') as f:
+    #            txt = f.read()
+    #            txt = txt[0:1000]
+    sentences = [['a', 'b', 'a', 'a', 'b', 'a', 'a', 'b', 'a', 'a', 'b', 'a', 'a', 'b', 'a', 'a', 'b', 'a']] #[['a', 'a', 'b', 'a', 'b', 'b', 'b', 'b', 'a', 'a', 'b', 'a', 'b'], ['b', 'b', 'a', 'b', 'a', 'a', 'a', 'a', 'b', 'b', 'a', 'b', 'a']]
+    unique_words = list(set(sentences[0]))
+    #search_buffer_ini = 'baa'
+    #naive_extend(search_buffer_ini, sentences, unique_words)
+    #entropy_naive_extend(sentences, unique_words, k=1)
+    entropy_better_extend(sentences, unique_words, k=0)
+
 
 run()
 # HERE WE ARE :)
