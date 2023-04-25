a8a4f7c94b3f78c9227cc4b6def5107b1f0286d6
estebanhramirez <128089275+estebanhramirez@users.noreply.github.com>
2023-04-25 19:38:51 +0000

    computes the pattern probability

--- PythonCode/Ngram/test.py
+++ PythonCode/Ngram/test.py
@@ -52,8 +52,8 @@ def correlation_information(n_gram_counts, probability_matrix, probability_matri
 
 
 
-def find_patterns_wrapper(sentences, unique_words, k, previous_n_gram):
-    print(previous_n_gram)
+def find_patterns_wrapper(sentences, unique_words, k, previous_n_gram, prev_prob):
+    print("P(",previous_n_gram,") = ", prev_prob)
     threshold = 0.7
 
     previous_n_gram_length = len(previous_n_gram)
@@ -90,7 +90,7 @@ def find_patterns_wrapper(sentences, unique_words, k, previous_n_gram):
         #print(relative_information(tuple(previous_n_gram+[nxt]), tuple(previous_n_gram[1:]+[nxt]), probability_matrix_plus1, probability_matrix))
         if np.log(1/prob)/prob < threshold:
             print(np.log(1/prob)/prob)
-            find_patterns_wrapper(sentences, unique_words, k, previous_n_gram+[nxt])
+            find_patterns_wrapper(sentences, unique_words, k, previous_n_gram+[nxt], prev_prob*prob)
 
 
 def find_patterns(sentences, unique_words, k, previous_n_gram):
@@ -98,7 +98,7 @@ def find_patterns(sentences, unique_words, k, previous_n_gram):
     n_plus1_gram_counts = count_n_grams(sentences, previous_n_gram_length+1)
     probability_matrix = make_probability_matrix(n_plus1_gram_counts, unique_words, k)
     for nxt in probability_matrix.columns:
-        find_patterns_wrapper(sentences, unique_words, k, previous_n_gram+[nxt])
+        find_patterns_wrapper(sentences, unique_words, k, previous_n_gram+[nxt], probability_matrix.loc[[tuple(previous_n_gram)]][nxt][0])
         print('----------------------------------------------------------------------')
 
 
@@ -116,8 +116,8 @@ def main():
     #txt = ['a', 'a', 'b', 'b', 'a', 'a', 'b', 'b', 'a', 'a', 'b', 'b', 'c', 'd', 'd', 'e', 'e', 'd', 'd', 'e', 'e']
     #txt = ['a', 'b', 'a', 'b', 'a', 'b', 'c', 'c', 'c', 'c', 'd', 'e', 'd', 'e', 'd', 'e', 'f'] # threshold = 0.70 & threshold = abs(average-entropy)
     #txt =  ['a', 'b', 'c', 'd']*2  # threshold = 0.95
-    #txt = ['1', 'b', 'b', '2', '9', '7', 'a', 'a', '3', 'b', 'b', '5', '1', 'a', 'a', '9', '4', 'b', 'b', '8', 'a', 'a', '6', '2', '0', 'b', 'b', 'b', '7', 'b', 'b']
-    txt = ['e', 's', 't', 'e', 'b', 'a', 'n', 'h', 'e', 'r', 'n', 'a', 'n', 'd', 'e', 'z', 'r', 'a', 'm', 'i', 'r', 'e', 'z']
+    txt = ['c', 'c', 'c', '1', 'a', 'a', '2', '9', '7', 'a', 'a', '3', 'a', 'a', '5', '1', 'a', 'a', '9', '4', 'a', 'a', '8', 'a', 'a', '6', '2', '0', 'b', 'b', 'b', '7', 'a', 'a']
+    #txt = ['e', 's', 't', 'e', 'b', 'a', 'n', 'h', 'e', 'r', 'n', 'a', 'n', 'd', 'e', 'z', 'r', 'a', 'm', 'i', 'r', 'e', 'z']
     sentences = [txt]
 
 
